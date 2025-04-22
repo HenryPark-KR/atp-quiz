3ce557c279130832ea72bfd692ebcc23de9c5cfb
HenryPark-KR <one1018@naver.com>
2025-04-22 20:48:18 +0900

    Add files via upload

---
+++ problems.js
@@ -0,0 +1,13 @@
+
+[
+  {
+    "question": "What is the primary purpose of a MOA?",
+    "choices": [
+      "A. Protect military aircraft from civil traffic.",
+      "B. Separate military training activities from IFR traffic.",
+      "C. Separate military training from both IFR and VFR traffic."
+    ],
+    "correct": "(B)",
+    "explanation": "MOAs are established to separate IFR traffic from military training operations."
+  }
+]
